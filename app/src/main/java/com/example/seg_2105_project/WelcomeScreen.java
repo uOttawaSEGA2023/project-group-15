d06c2819d8fdc919165804ff09b906c2705dc206
Sedra Hamida <shami099@uottawa.ca>
2023-10-13 15:02:43 -0400

    welcome screen

---
+++ app/src/main/java/com/example/seg_2105_project/WelcomeScreen.java
@@ -0,0 +1,29 @@
+package com.example.seg_2105_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeScreen extends AppCompatActivity {
+    Button signUpButton;
+    Button signInButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+        signUpButton = findViewById(R.id.button2);
+        signInButton = findViewById(R.id.button);
+    }
+
+    public void onClick_SingIn(View view){
+        startActivity(new Intent(getApplicationContext(),SignIn.class));
+    }
+
+    /*public void onClick_SignUp(View view){
+        startActivity(new Intent(getApplicationContext(),SignUp.class));
+    Remember to add it with the correct name for the sign up screen once connor finishes it
+    }*/
+}

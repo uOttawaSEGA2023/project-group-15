6b4410b472ed56992f900070cdf03196a7044d4f
shazia-dewan <shazia.dewan04@gmail.com>
2023-10-14 15:45:05 -0400

    finished doctor sign up with firebase

--- app/src/main/java/com/example/seg_2105_project/Doctor_SignUp.java
+++ app/src/main/java/com/example/seg_2105_project/Doctor_SignUp.java
@@ -2,12 +2,18 @@ package com.example.seg_2105_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -86,147 +92,209 @@ public class Doctor_SignUp extends AppCompatActivity {
         boolean isValid = true;
 
         //Check if each input is correct or empty
-        if (user.firstName.equals("")) {
+        if (user.getFirstName().equals("")) {
             //Set visibility of error message to visible
-            TextView text = findViewById(R.id.textViewFirstNameError);
+            TextView text = findViewById(R.id.first_name);
             text.setVisibility(view.VISIBLE);
 
             //Set visibility of label to invisible
-            text = findViewById(R.id.textViewFirstName);
+            text = findViewById(R.id.first_name_enter);
             text.setVisibility(view.INVISIBLE);
 
             isValid = false;
         }
         else {
             //Reset visibilities
-            TextView text = findViewById(R.id.textViewFirstName);
+            TextView text = findViewById(R.id.first_name_enter);
             text.setVisibility(view.VISIBLE);
 
-            text = findViewById(R.id.textViewFirstNameError);
+            text = findViewById(R.id.first_name);
             text.setVisibility(view.INVISIBLE);
 
         }
-        if (user.lastName.equals("")) {
+        if (user.getLastName().equals("")) {
             //Set visibility of error message to visible
-            TextView text = findViewById(R.id.textViewLastNameError);
+            TextView text = findViewById(R.id.last_name);
             text.setVisibility(view.VISIBLE);
 
             //Set visibility of label to invisible
-            text = findViewById(R.id.textViewLastName);
+            text = findViewById(R.id.last_name_enter);
             text.setVisibility(view.INVISIBLE);
 
             isValid = false;
         }
         else {
             //Reset visibilities
-            TextView text = findViewById(R.id.textViewLastName);
+            TextView text = findViewById(R.id.last_name_enter);
             text.setVisibility(view.VISIBLE);
 
-            text = findViewById(R.id.textViewLastNameError);
+            text = findViewById(R.id.last_name);
             text.setVisibility(view.INVISIBLE);
 
         }
-        if (user.email.equals("") || !user.email.contains("@")) {
+        if (user.getEmail().equals("") || !user.getEmail().contains("@")) {
             //Set visibility of error message to visible
-            TextView text = findViewById(R.id.textViewEmailError);
+            TextView text = findViewById(R.id.email_address_enter);
             text.setVisibility(view.VISIBLE);
 
             //Set visibility of label to invisible
-            text = findViewById(R.id.textViewEmail);
+            text = findViewById(R.id.email_address);
             text.setVisibility(view.INVISIBLE);
 
             //Clear input
-            EditText input = findViewById(R.id.editTextEmailAddress);
+            EditText input = findViewById(R.id.email_address_enter);
             input.setText("");
 
             isValid = false;
         }
         else {
             //Reset visibilities
-            TextView text = findViewById(R.id.textViewEmail);
+            TextView text = findViewById(R.id.email_address);
             text.setVisibility(view.VISIBLE);
 
-            text = findViewById(R.id.textViewEmailError);
+            text = findViewById(R.id.email_address_enter);
             text.setVisibility(view.INVISIBLE);
         }
-        if (user.password.equals("")) {
+        if (user.getPassword().equals("")) {
             //Set visibility of error message to visible
-            TextView text = findViewById(R.id.textViewPasswordError);
+            TextView text = findViewById(R.id.account_password_enter);
             text.setVisibility(view.VISIBLE);
 
             //Set visibility of label to invisible
-            text = findViewById(R.id.textViewPassword);
+            text = findViewById(R.id.account_pass);
             text.setVisibility(view.INVISIBLE);
 
             isValid = false;
         }
         else {
             //Reset visibilities
-            TextView text = findViewById(R.id.textViewPassword);
+            TextView text = findViewById(R.id.account_pass);
             text.setVisibility(view.VISIBLE);
 
-            text = findViewById(R.id.textViewPasswordError);
+            text = findViewById(R.id.account_password_enter);
             text.setVisibility(view.INVISIBLE);
         }
-        if (user.phoneNumber == -1 || user.phoneNumber < 100000000 || user.phoneNumber > 999999999) {
+        if (user.getPhoneNumber() == -1 || user.getPhoneNumber()< 100000000 || user.getPhoneNumber() > 999999999) {
             //Set visibility of error message to visible
-            TextView text = findViewById(R.id.textViewPhoneNumberError);
+            TextView text = findViewById(R.id.phone_number_enter);
             text.setVisibility(view.VISIBLE);
 
             //Set visibility of label to invisible
-            text = findViewById(R.id.textViewPhoneNumber);
+            text = findViewById(R.id.phone_number);
             text.setVisibility(view.INVISIBLE);
 
             isValid = false;
         }
         else {
             //Reset visibilities
-            TextView text = findViewById(R.id.textViewPhoneNumber);
+            TextView text = findViewById(R.id.phone_number);
             text.setVisibility(view.VISIBLE);
 
-            text = findViewById(R.id.textViewPhoneNumberError);
+            text = findViewById(R.id.phone_number_enter);
             text.setVisibility(view.INVISIBLE);
 
         }
-        if (user.address.equals("")) {
+        if (user.getAddress().equals("")) {
             //Set visibility of error message to visible
-            TextView text = findViewById(R.id.textViewAddressError);
+            TextView text = findViewById(R.id.address_enter);
             text.setVisibility(view.VISIBLE);
 
             //Set visibility of label to invisible
-            text = findViewById(R.id.textViewAddress);
+            text = findViewById(R.id.address);
             text.setVisibility(view.INVISIBLE);
 
             isValid = false;
         }
         else {
             //Reset visibilities
-            TextView text = findViewById(R.id.textViewAddress);
+            TextView text = findViewById(R.id.address);
             text.setVisibility(view.VISIBLE);
 
-            text = findViewById(R.id.textViewAddressError);
+            text = findViewById(R.id.address_enter);
             text.setVisibility(view.INVISIBLE);
         }
-        if (user.employeeNumber == -1 || user.healthCardNumber < 1000000000) {
+        if (user.get_employee_number()== -1 || user.get_employee_number() < 1000000000) {
             //Set visibility of error message to visible
-            TextView text = findViewById(R.id.textViewHealthNumberError);
+            TextView text = findViewById(R.id.employee_num_enter);
             text.setVisibility(view.VISIBLE);
 
             //Set visibility of label to invisible
-            text = findViewById(R.id.textViewHealthNumber);
+            text = findViewById(R.id.employee_num);
             text.setVisibility(view.INVISIBLE);
 
             isValid = false;
         }
         else {
             //Reset visibilities
-            TextView text = findViewById(R.id.textViewHealthNumber);
+            TextView text = findViewById(R.id.employee_num);
             text.setVisibility(view.VISIBLE);
 
-            text = findViewById(R.id.textViewHealthNumberError);
+            text = findViewById(R.id.employee_num_enter);
+            text.setVisibility(view.INVISIBLE);
+        }
+        if (user.get_specialties().equals("")) {
+            //Set visibility of error message to visible
+            TextView text = findViewById(R.id.specialty_enter);
+            text.setVisibility(view.VISIBLE);
+
+            //Set visibility of label to invisible
+            text = findViewById(R.id.specialty);
+            text.setVisibility(view.INVISIBLE);
+
+            isValid = false;
+        }
+        else {
+            //Reset visibilities
+            TextView text = findViewById(R.id.specialty);
+            text.setVisibility(view.VISIBLE);
+
+            text = findViewById(R.id.specialty_enter);
             text.setVisibility(view.INVISIBLE);
         }
         return isValid;
 
     }
+
+    /*
+    Authenticate user info
+     */
+    private void userAuthentication(Doctor doctor) {
+        auth.createUserWithEmailAndPassword(doctor.getEmail(), doctor.getPassword())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        //Check if authentication was successful
+                        if (task.isSuccessful()) {
+                            verifyEmail();
+
+                        } else {
+                            // If unsuccessful, display a message to the user.
+                            Toast.makeText(Doctor_SignUp.this, "This email already has an account",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    /*
+    Send email to user to verify their email
+     */
+    private void verifyEmail() {
+        FirebaseUser user = auth.getCurrentUser();
+        //Send email to user
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(Task<Void> task) {
+                        //Send message to user that email has been sent and change screen
+                        Toast.makeText(getApplicationContext(), "Verification email sent",
+                                Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), SignIn.class); //TODO switch to sign in screen
+                        startActivity(intent);
+
+                    }
+                });
+    }
+
+}
 }

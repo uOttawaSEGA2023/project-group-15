464a54bea95af75e5301a0c3726098a6869ffe30
mpalcz <megg.palcz@gmail.com>
2023-10-14 14:31:29 -0400

    Welcome screen skeletons

--- app/src/main/java/com/example/seg_2105_project/DoctorScreen.java
+++ app/src/main/java/com/example/seg_2105_project/DoctorScreen.java
@@ -2,13 +2,26 @@ package com.example.seg_2105_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DoctorScreen extends AppCompatActivity {
 
+    TextView welcomeMessage;
+    String name = ///Doctor name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_screen);
+        welcomeMessage = findViewById(R.id.welcomeMessage);
+        welcomeMessage.setText("Welcome, " + name + "!");
+    }
+
+    public void onClickSignOutButton(View view) {
+        //Transition to home screen of app
+        Intent intent = new Intent(getApplicationContext(), WelcomeScreen.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/seg_2105_project/PatientScreen.java
+++ app/src/main/java/com/example/seg_2105_project/PatientScreen.java
@@ -2,13 +2,27 @@ package com.example.seg_2105_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PatientScreen extends AppCompatActivity {
+
+    TextView welcomeMessage;
+    String name = ///patient name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_screen);
+        welcomeMessage = findViewById(R.id.welcomeMessage);
+        welcomeMessage.setText("Welcome, " + name + "!");
+    }
+
+    public void onClickSignOutButton(View view) {
+        //Transition to home screen of app
+        Intent intent = new Intent(getApplicationContext(), WelcomeScreen.class);
+        startActivity(intent);
+
     }
 }

9358e4f983472a2f54fb0309e1fb485d2c0ac23e
connorlucid <90726995+connorlucid@users.noreply.github.com>
2023-10-14 21:59:59 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/seg_2105_project/MainActivity.java
+++ app/src/main/java/com/example/seg_2105_project/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(getApplicationContext(), SignUpPage.class);
+        Intent intent = new Intent(getApplicationContext(), WelcomeScreen.class);
         startActivity(intent);
     }
 }

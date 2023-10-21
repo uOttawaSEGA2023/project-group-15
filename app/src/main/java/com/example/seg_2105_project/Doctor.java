ecc3bfbe1aa64c9685907753b9d6534897c0d38a
Anika Pathak <anika.pathak@gmail.com>
2023-10-21 17:23:43 -0400

    Added getPatients and getDoctors methods

--- app/src/main/java/com/example/seg_2105_project/Doctor.java
+++ app/src/main/java/com/example/seg_2105_project/Doctor.java
@@ -1,5 +1,7 @@
 package com.example.seg_2105_project;
 
+import com.google.firebase.database.DataSnapshot;
+
 import java.util.ArrayList;
 
 public class Doctor extends User {
@@ -20,6 +22,34 @@ public class Doctor extends User {
 
     public ArrayList<String> get_specialties() {
         return specialties;
+    }
+
+    /*
+    Gives a list of doctors with a certain registration status from firebase
+    @param  status                      Registration status of doctors
+    @param  dataSnapshot                DataSnapshot of doctor information in firebase
+    @return                             An ArrayList of doctors
+    @throws IllegalArguementException   if dataSnapshot is null or doesn't contain a snapshot for the doctors
+     */
+    public static ArrayList<User> getDoctors(RegistrationStatus status, DataSnapshot dataSnapshot) {
+
+        ArrayList<User> doctors = new ArrayList<User>();
+
+        //Make sure dataSnapshot isn't null and contains the Doctor path
+        if (dataSnapshot.exists() && dataSnapshot.getRef().getKey().equals("Doctors")) {
+            for (DataSnapshot doctor : dataSnapshot.getChildren()) {
+                Doctor d = doctor.getValue(Doctor.class);
+                //Check registration status of doctor and add it to list if it matches
+                if (d.getRegistrationStatus() == status) {
+                    doctors.add(d);
+                }
+            }
+        }
+        else {
+            throw new IllegalArgumentException("dataSnapshot should not be null and should be from the Doctor path");
+        }
+        return doctors;
+
     }
 
 
--- app/src/main/java/com/example/seg_2105_project/Patient.java
+++ app/src/main/java/com/example/seg_2105_project/Patient.java
@@ -1,5 +1,9 @@
 package com.example.seg_2105_project;
 
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.ArrayList;
+
 public class Patient extends User {
 
     private long healthCardNumber;
@@ -13,4 +17,33 @@ public class Patient extends User {
     public long getHealthCardNumber() {
         return healthCardNumber;
     }
+
+    /*
+    Gives a list of patients with a certain registration status from firebase
+    @param  status                      Registration status of doctors
+    @param  dataSnapshot                DataSnapshot of patient information in firebase
+    @return                             An ArrayList of doctors
+    @throws IllegalArguementException   if dataSnapshot is null or doesn't contain a snapshot for the patients
+     */
+    public static ArrayList<User> getPatients(RegistrationStatus status, DataSnapshot dataSnapshot) {
+
+        ArrayList<User> patients = new ArrayList<User>();
+
+        //Make sure dataSnapshot isn't null and contains the Patient path
+        if (dataSnapshot.exists() && dataSnapshot.getRef().getKey().equals("Patients")) {
+            for (DataSnapshot patient : dataSnapshot.getChildren()) {
+                Patient p = patient.getValue(Patient.class);
+                //Check registration status of doctor and add it to list if it matches
+                if (p.getRegistrationStatus() == status) {
+                    patients.add(p);
+                }
+            }
+        }
+        else {
+            throw new IllegalArgumentException("dataSnapshot should not be null and should be from the Patient path");
+        }
+        return patients;
+
+    }
+
 }

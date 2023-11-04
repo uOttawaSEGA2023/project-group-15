f217ebbb04bf35f153d32df91dfe73b61eab5e9f
Anika Pathak <anika.pathak@gmail.com>
2023-11-04 13:14:41 -0400

    Updated Doctor class, added Appointment and Shift class

---
+++ app/src/main/java/com/example/seg_2105_project/Appointment.java
@@ -0,0 +1,7 @@
+package com.example.seg_2105_project;
+
+public class Appointment {
+
+
+
+}
--- app/src/main/java/com/example/seg_2105_project/Doctor.java
+++ app/src/main/java/com/example/seg_2105_project/Doctor.java
@@ -1,5 +1,7 @@
 package com.example.seg_2105_project;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -9,31 +11,35 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class Doctor extends User {
-    int employee_number;
-    ArrayList<String> specialties;
+    private int employee_number;
+    private ArrayList<String> specialties;
+    private ArrayList<Appointment> appointments;
+    private ArrayList<Shift> shifts;
+    private boolean autoApprove;
 
     public Doctor() {}
     public Doctor(String firstName, String lastName, String email, String password, long phoneNumber, String address, int employee_number, ArrayList<String> specialties) {
         super(firstName, lastName, email, password, phoneNumber, address);
         this.employee_number = employee_number;
         this.specialties = specialties;
+        this.appointments = new ArrayList<>();
+        this.shifts = new ArrayList<>();
+        this.autoApprove = false;
     }
 
-    public int get_employee_number() {
-        return employee_number;
-
-    }
-
-    public ArrayList<String> get_specialties() {
-        return specialties;
-    }
+    /**GETTERS**/
+    public int get_employee_number() { return employee_number; }
+    public ArrayList<String> get_specialties() { return specialties; }
+    public ArrayList<Appointment> getAppointments() { return appointments; }
+    public ArrayList<Shift> getShifts() { return shifts; }
+    public boolean getAutoApprove() { return autoApprove; }
 
     /*
-    Gives a list of doctors with a certain registration status from firebase
-    @param  status                      Registration status of doctors
-    @param  dataSnapshot                DataSnapshot of doctor information in firebase
-    @return                             An ArrayList of doctors
-    @throws IllegalArguementException   if dataSnapshot is null or doesn't contain a snapshot for the doctors
+    * Gives a list of doctors with a certain registration status from firebase
+    * @param  status                      Registration status of doctors
+    * @param  dataSnapshot                DataSnapshot of doctor information in firebase
+    * @return                             An ArrayList of doctors
+    * @throws IllegalArguementException   if dataSnapshot is null or doesn't contain a snapshot for the doctors
      */
     public static ArrayList<User> getDoctors(RegistrationStatus status, DataSnapshot dataSnapshot) {
 
@@ -56,6 +62,9 @@ public class Doctor extends User {
 
     }
 
+    /**SETTERS**/
+    public void setAutoApprove(boolean autoApprove) { this.autoApprove = autoApprove; }
+
     /*
      * Acts as a setter and changes the registration status in Firebase
      */
@@ -76,7 +85,7 @@ public class Doctor extends User {
                         //Change status
                         DatabaseReference reference = doctor.getRef();
                         reference.child("registrationStatus").setValue(registrationStatus);
-
+                        break;
                     }
                 }
 
@@ -88,21 +97,10 @@ public class Doctor extends User {
 
     }
 
-    /*
-    Reads through the database for doctors and return Doctor object if found
-     */
-    public static Doctor getDoctor(String email, String password, DataSnapshot dataSnapshot) {
-        for (DataSnapshot doctorSnapshot : dataSnapshot.getChildren()) {
-            Doctor doctor = doctorSnapshot.getValue(Doctor.class);
-            if (doctor.getEmail().equals(email) && doctor.getPassword().equals(password)) {
-                return doctor;
-            }
-        }
-        return null;
-    }
+    /**OTHER METHODS**/
 
     /*
-    Displays all information of this doctor
+    * Displays all information of this doctor
      */
     @Override
     public String display() {
@@ -114,6 +112,52 @@ public class Doctor extends User {
         }
         return display;
 
+    }
+
+    /*
+     * Adds an appointment to this doctors list of appointments and updates Firebase
+     */
+    public void addAppointment(Appointment a) {
+        this.appointments.add(a);
+
+        //Update firebase
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Doctors");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Look for this doctor in Firebase
+                for (DataSnapshot doctorSnapshot : snapshot.getChildren()) {
+
+                    Doctor doctor = doctorSnapshot.getValue(Doctor.class);
+                    if (doctor.getEmail().equals(getEmail())) {
+                        //Change appointments
+                        DatabaseReference reference = doctorSnapshot.getRef();
+                        reference.child("appointments").setValue(appointments);
+                        break;
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+
+    }
+
+    /**CLASS METHODS**/
+
+    /*
+    * Reads through the database for doctors and return Doctor object if found
+     */
+    public static Doctor getDoctor(String email, String password, DataSnapshot dataSnapshot) {
+        for (DataSnapshot doctorSnapshot : dataSnapshot.getChildren()) {
+            Doctor doctor = doctorSnapshot.getValue(Doctor.class);
+            if (doctor.getEmail().equals(email) && doctor.getPassword().equals(password)) {
+                return doctor;
+            }
+        }
+        return null;
     }
 
 

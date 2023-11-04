8f3925cc61e86905e0fa6bd7122842df15d4b2ec
Anika Pathak <anika.pathak@gmail.com>
2023-11-04 14:01:02 -0400

    Added compareTo in Time class

--- app/src/main/java/com/example/seg_2105_project/Time.java
+++ app/src/main/java/com/example/seg_2105_project/Time.java
@@ -1,6 +1,6 @@
 package com.example.seg_2105_project;
 
-public class Time {
+public class Time implements Comparable {
 
     private int minutes;
     private int hours;
@@ -22,4 +22,26 @@ public class Time {
         return hours + ":" + minutes;
     }
 
+    /*
+    * Returns a negative number if the other time is before this time, a positive number
+    * if the other time is after this time, and 0 if they are the same
+     */
+    @Override
+    public int compareTo(Object o) {
+
+        Time other = (Time) o;
+        if (other.getHours() < hours)
+            return -1;
+        else if (other.getHours() > hours)
+            return 1;
+        else {
+            if (other.getMinutes() < minutes)
+                return -1;
+            else if (other.getMinutes() > minutes)
+                return 1;
+            else
+                return 0;
+        }
+
+    }
 }

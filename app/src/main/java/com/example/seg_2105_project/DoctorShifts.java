4eab615a7aeeaec80b84ad4a959d306f883ba606
connorlucid <90726995+connorlucid@users.noreply.github.com>
2023-11-12 00:24:01 -0500

    Update DoctorShifts.java

--- app/src/main/java/com/example/seg_2105_project/DoctorShifts.java
+++ app/src/main/java/com/example/seg_2105_project/DoctorShifts.java
@@ -87,8 +87,11 @@ public class DoctorShifts extends AppCompatActivity {
                     shiftsList.clear();
 
                     for (DataSnapshot shiftSnapshot : dataSnapshot.getChildren()) {
-                        String shift = shiftSnapshot.getValue(String.class);
-                        shiftsList.add(shift);
+                        Shift shift = shiftSnapshot.getValue(Shift.class);
+                        if (shift != null) {
+                            // Assuming Shift class has a proper toString() method
+                            shiftsList.add(shift.toString());
+                        }
                     }
 
                     adapter.notifyDataSetChanged();
@@ -101,4 +104,5 @@ public class DoctorShifts extends AppCompatActivity {
             });
         }
     }
+
 }

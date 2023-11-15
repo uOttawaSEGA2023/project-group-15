0c76baa72ea4917f8298813f1cd0c22ddd526d92
mpalcz <megg.palcz@gmail.com>
2023-11-15 09:53:07 -0500

    delete unused import

--- app/src/main/java/com/example/seg_2105_project/DoctorShifts.java
+++ app/src/main/java/com/example/seg_2105_project/DoctorShifts.java
@@ -25,7 +25,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 public class DoctorShifts extends AppCompatActivity {
 

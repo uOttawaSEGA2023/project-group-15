5a3df7b6e8f36a10413fbdb20a3a8f5334d7f7e2
Anika Pathak <anika.pathak@gmail.com>
2023-11-24 23:06:17 -0500

    Fixed search bar display

--- app/src/main/java/com/example/seg_2105_project/Frontend/PatientActivities/PatientBookAppointment.java
+++ app/src/main/java/com/example/seg_2105_project/Frontend/PatientActivities/PatientBookAppointment.java
@@ -2,6 +2,7 @@ package com.example.seg_2105_project.Frontend.PatientActivities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Filter;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.example.seg_2105_project.Backend.Appointment;
 import com.example.seg_2105_project.Backend.Doctor;
@@ -40,6 +42,9 @@ public class PatientBookAppointment extends AppCompatActivity {
         setContentView(R.layout.activity_patient_book_appointment2);
 
         listViewDoctors = findViewById(R.id.listViewDoctors);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         //obtaining references to the database
         FirebaseDatabase db = FirebaseDatabase.getInstance();
@@ -133,20 +138,25 @@ public class PatientBookAppointment extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        System.out.println("inflating menu");
         getMenuInflater().inflate(R.menu.menu, menu);
 
         MenuItem menuItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) menuItem.getActionView();
+        SearchView searchView = new SearchView(getApplicationContext());
         searchView.setQueryHint("Type here to search for doctor by specialty");
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
+                Toast.makeText(getApplicationContext(), "pls submit", Toast.LENGTH_SHORT).show();
+                arrayAdapterDoctor.getFilter().filter(s);
                 return false;
+
             }
 
             @Override
             public boolean onQueryTextChange(String s) {
+                Toast.makeText(getApplicationContext(), "pls", Toast.LENGTH_SHORT).show();
                 arrayAdapterDoctor.getFilter().filter(s);
                 return false;
             }
